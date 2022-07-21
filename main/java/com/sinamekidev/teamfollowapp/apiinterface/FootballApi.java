5ebe722b6b3a7e14b39281a21b8a1fec2966a558
sarpdora23 <69794292+sarpdora23@users.noreply.github.com>
2022-07-21 23:35:41 +0300

    Update FootballApi.java

--- main/java/com/sinamekidev/teamfollowapp/apiinterface/FootballApi.java
+++ main/java/com/sinamekidev/teamfollowapp/apiinterface/FootballApi.java
@@ -8,7 +8,6 @@ import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 public interface FootballApi {
-   // @GET("search.json?q={league_name}+teams&api_key=30bcb7e2d495b6bae20de36ba97330758c433b04d318b38aa358efc5f366b5ce")
     @GET("search.json")
     Call<QueryModel> getQueryModel(@Query("q") String league_name,@Query("api_key") String api_key);
 }
